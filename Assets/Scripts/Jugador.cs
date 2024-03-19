a2eabb7fe702a27a63d662f76445b04abf96c940
Geralmp <geraldinpuerta2001@gmail.com>
2024-03-19 08:16:39 -0500

    Avances3_sonido

--- Assets/Scripts/Jugador.cs
+++ Assets/Scripts/Jugador.cs
@@ -6,6 +6,9 @@ public class Jugador : MonoBehaviour
     public float JumpSpeed;
     private bool isDead = false;
     private bool isWinner = false;
+    public AudioClip Sound;
+    public AudioClip Sound2;
+    public AudioClip GameoverSound;
 
     private Rigidbody rb;
 
@@ -19,18 +22,21 @@ public class Jugador : MonoBehaviour
         if (collision.gameObject.CompareTag("Plataforma"))
         {
             rb.AddForce(Vector3.up * JumpSpeed, ForceMode.Impulse);
+            Camera.main.GetComponent<AudioSource>().PlayOneShot(Sound);
         }
 
         if (collision.gameObject.CompareTag("Plataforma_perder") && !isDead) 
         {
             isDead = true;
             GameOver();
+            Camera.main.GetComponent<AudioSource>().PlayOneShot(GameoverSound);
         }
 
         if (collision.gameObject.CompareTag("winnerr") && !isDead) 
         {
             isWinner = false;
             winner();
+            Camera.main.GetComponent<AudioSource>().PlayOneShot(Sound2);
         }
     }
 
